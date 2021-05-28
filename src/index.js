2241f4c27f84d9b527245ad8b7edde27f938d512
Alexey <kiselevich.alexey97@gmail.com>
2021-05-28 23:52:03 +0300

    firstPage

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './node_modules/app/App';
+
+
+ReactDOM.render(
+    <App/>
+  ,
+  document.getElementById('root')
+)
+
+
