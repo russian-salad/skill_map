0c184c8f593ca2a842ff6fffbbbf8cbd7d28e647
Alexey <kiselevich.alexey97@gmail.com>
2021-05-29 00:43:13 +0300

    -

---
+++ src/node_modules/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from './footer.module.css'
+
+export default function Footer() {
+    return (
+        <footer className={s.footer}>
+            <h2>Footer </h2>
+        </footer>
+    )
+}
