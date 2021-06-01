d7279b9d2cc12261b91b418cb83f16020c730722
Alexey <kiselevich.alexey97@gmail.com>
2021-06-01 14:42:35 +0300

    добавлен запрос к API(мок)

--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -1,62 +1,61 @@
-import React from 'react'
-import Search from 'components/search'
-import Button from 'components/button'
-import s from 'pages/homePage/homepage.module.css'
-import Pagination from "components/pagination";
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import {Link} from 'react-router-dom'
+import Search from "components/search"
+import Button from "components/button"
+import s from "pages/homePage/homepage.module.css"
+import Pagination from "components/pagination"
 
-
-    function SkillCard(props) {
-            //  const { emblemUrl, id, code, name, area } = props.card;
-                return (
-                //   <Link
-                //     to={{
-                //       pathname: props.type + "/" + id,
-                //     }}
-                //   >
-                    <div className={s.card}>
-                      {/* <div className="content"> */}
-                        {/* <div className="card-img">
-                          <img src={emblemUrl || leagueLogo[code]} alt={name} />
+function SpecCard(props) {
+   const { name,username } = props.data
+  return (
+      <Link to={{ pathname:'#'}} >
+    <div className={s.card}>
+      {/* <div className="content"> */}
+      {/* <div className="card-img">
+                          <img src={emblemUrl alt={name} />
                         </div> */}
-                        <div className="card-info">
-                          <h3>Load</h3>
-                          <p>load</p>
-                        </div>
-                      {/* </div> */}
-                    </div>
-                //   {/* </Link> */}
+      <div className="card-info">
+        <h3>{name}</h3>
+        <p>{username}</p>
+      </div>
+      {/* </div> */}
+    </div>
+     </Link>
+  );
+}
 
 
-        )
-    }
-export default function HomePage (props) {
-    return(
-        <>
-         <div className={s.search}>
-             <Search/>
-             <Button text='Найти'/>
-         </div>
-         <div className={s.flex}>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-            <SkillCard></SkillCard>
-         </div>
-         <div style={{display:'flex',justifyContent:'center',margin:10}}>
-                <Pagination/>
-         </div>
-        </>
-    )
+export default function HomePage(props) {
+  const [data, setData] = useState([])
+  
+  const url = "https://jsonplaceholder.typicode.com/users"
+  const token = ""
+
+
+  useEffect(async () => {
+    await axios
+      .get(url, {
+        // headers: { "X-Auth-Token": token },
+        dataType: "json",
+        type: "GET",
+      })
+      .then((response) => setData(response.data))
+      .catch((e) => console.log(e))
+  },[])
+const specList =data.map(item=><SpecCard data={item}/>)
+
+  return (
+    <>
+      <div className={s.search}>
+        <Search /> <Button text="Найти" />
+      </div>
+      <div className={s.flex}>
+        {specList}
+      </div>
+      <div style={{ display: "flex", justifyContent: "center", margin: 10 }}>
+        <Pagination />
+      </div>
+    </>
+  );
 }
