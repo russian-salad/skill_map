37d5a5e9d3b5d92557c5f5016f554cb186cd9f28
Alexey <kiselevich.alexey97@gmail.com>
2021-06-03 17:14:39 +0300

    добавлена функция пагинации

--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -1,43 +1,42 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Pagination from "@material-ui/lab/Pagination";
 
 import SpecCard from "pages/homePage/specCard";
 import Search from "components/search";
 import Button from "components/button";
 import s from "pages/homePage/homepage.module.css";
-import Pagination from "components/pagination";
 
 export default function HomePage() {
-  
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
   const [list, setList] = useState([]);
-
+  const [page, setPage] = useState(1);
 
   const url = "https://jsonplaceholder.typicode.com/users";
   const token = "";
 
-  useEffect( () => {
-   const fetchSpecialization = async()=>{
-    const res = await axios.get(url,
-      //  {  headers: { "X-Auth-Token": token }  }
-      )
-        setData(res.data);
-        setList(res.data);
-      }
-      fetchSpecialization()
-}
-  , []);
+  useEffect(() => {
+    const fetchSpecialization = async () => {
+      const res = await axios.get(
+        url
+        //  {  headers: { "X-Auth-Token": token }  }
+      );
+      setData(res.data);
+      setList(res.data);
+    };
+    fetchSpecialization();
+  }, []);
 
-  const searchChange = (event) =>{
-    setSearch(event.target.value)
-    if(event.target.value === '' ) { setSearch(event.target.value);searchButtonClick()}
-  }
-  
-
+  const searchChange = (event) => {
+    setSearch(event.target.value);
+    if (event.target.value === "") {
+      setSearch(event.target.value);
+      searchButtonClick();
+    }
+  };
 
   function searchButtonClick() {
-    console.log(search);
     setList(
       data.filter((item) =>
         item.name.toLowerCase().includes(search.toLowerCase())
@@ -45,14 +44,18 @@ export default function HomePage() {
     );
   }
 
-
   function handleSubmit(e) {
     e.preventDefault();
     searchButtonClick();
   }
-  
 
   const specList = list.map((item) => <SpecCard key={item.id} data={item} />);
+
+  const range = 3;
+  const pageList = specList.slice(
+    (page - 1) * range,
+    (page - 1) * range + range
+  );
 
   return (
     <>
@@ -60,9 +63,13 @@ export default function HomePage() {
         <Search onChange={searchChange} />
         <Button handleClick={searchButtonClick} text="Найти" />
       </form>
-      <div className={s.flex}>{specList}</div>
+      <div className={s.flex}>{pageList}</div>
       <div className={s.flex}>
-        <Pagination/>
+        <Pagination
+        color='primary'
+          onChange={(event, value) => setPage(value)}
+          count={Math.ceil(specList.length / range)}
+        />
       </div>
     </>
   );
