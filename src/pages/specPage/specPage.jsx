bbf56c7f7bc9419539306a613dd4ec88b0e1ce30
Alexey <kiselevich.alexey97@gmail.com>
2021-06-04 16:32:05 +0300

    спинер для стр.специализации

--- src/pages/specPage/specPage.jsx
+++ src/pages/specPage/specPage.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Accordion from "components/accordion";
 import axios from "axios";
+import { CircularProgress } from "@material-ui/core";
 
 export default function (props) {
   const [data, setData] = useState([]);
-
+  const [loading,setLoading] = useState(false)
   const url = "https://jsonplaceholder.typicode.com/posts";
   useEffect(() => {
     const fetchSkills = async () => {
@@ -13,10 +14,18 @@ export default function (props) {
     };
     fetchSkills()
   }, []);
-
-  return (
+  useEffect(()=>{setLoading(!loading)},[data])
+if(loading){
+  return <div style={{display:'flex',justifyContent:'center',marginTop:50}}>
+    <CircularProgress
+   size={150}
+   thickness={2}    
+  />
+  </div>
+}else{
+    return (
     <>
       <Accordion data={data}></Accordion>
     </>
-  );
+  );}
 }
