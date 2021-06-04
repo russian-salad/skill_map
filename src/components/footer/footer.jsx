0dd4951fe9cbd968fed0c1c5bf7b0589877cbcc2
Alexey <kiselevich.alexey97@gmail.com>
2021-06-04 16:56:09 +0300

    стили для футера

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import s from './footer.module.css'
 
 export default function Footer() {
+    const date =new Date().getFullYear()
     return (
         <footer className={s.footer}>
-            <h2>Footer </h2>
+            <p> © YoursRoadMap {date} </p>
         </footer>
     )
 }
